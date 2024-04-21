9f977b47dd98b955e53df3f9b1a0f65cab4d8bed
hamed18 <hamedcnec@gmail.com>
2024-04-21 16:09:55 +0600

    final code

--- scripts/utility.js
+++ scripts/utility.js
@@ -1,4 +1,4 @@
-// This are reusable js function associated with DOM and Event Handler.
+// Reusable Functions
 function hideElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('hidden');
@@ -18,6 +18,7 @@ function removeBackgroundColorById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('bg-orange-400');
 }
+
 
 function getTextElementValueById(elementId){
     const element = document.getElementById(elementId);
@@ -48,18 +49,17 @@ function getARandomAlphabet(){
     return alphabet;
 }
 
-/* 
-function getARandomAlphabet() {
-    / get or create an alphabet array
-     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
-     const alphabets = alphabetString.split('');
-     console.log(alphabets);
-    / get a random index between 0 -25
-     const randomNumber = Math.random() * 25;
-     const index = Math.round(randomNumber);
+// function getARandomAlphabet() {
+//     // get or create an alphabet array
+//     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+//     const alphabets = alphabetString.split('');
+//     // console.log(alphabets);
+
+//     // get a random index between 0 -25
+//     const randomNumber = Math.random() * 25;
+//     const index = Math.round(randomNumber);
     
-     const alphabet = alphabets[index];
-     console.log(index, alphabet);
-     return alphabet;
-}
-*/
+//     const alphabet = alphabets[index];
+//     // console.log(index, alphabet);
+//     return alphabet;
+// }
