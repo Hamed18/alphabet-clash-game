86888b9efdf8ecb335007ac93730e815162c9520
hamed18 <hamedcnec@gmail.com>
2024-04-21 15:21:39 +0600

    some reusable functions are added

---
+++ styles/scripts/utility.js
@@ -0,0 +1,20 @@
+// This are reusable js function associated with DOM and Event Handler.
+function hideElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
+}
+
+function showElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
+
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
+
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
